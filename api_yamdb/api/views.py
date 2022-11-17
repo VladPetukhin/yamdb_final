97965b98f8271b3254f5309cabd1ca191b19be56
VladPetukhin <104086673+VladPetukhin@users.noreply.github.com>
2022-11-17 15:50:33 +0300

    as

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 from rest_framework.pagination import PageNumberPagination
-
 from reviews.models import Category, Comment, Genre, Review, Title
 
 from .filters import TitleFilter
