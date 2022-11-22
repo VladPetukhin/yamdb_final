0190fa704c26dcad68e159dacbc2d35d1db4aa19
VladPetukhin <104086673+VladPetukhin@users.noreply.github.com>
2022-11-22 16:11:48 +0300

    second

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
+    'rest_framework',
+    'django_filters',
     'django_extensions',
     'api',
     'reviews',
